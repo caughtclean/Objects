0f56c06a3a4496b8659286c2d1cf7fbeef32c71c
Nathan Eyerley <eyerleyn@gmail.com>
2016-11-02 21:01:12 +0000

    Exercise 1

---
+++ count-letters.js
@@ -0,0 +1,23 @@
+
+function countLetters(characters) {
+    var obj = {}
+    for (var i = 0; i < characters.length; i += 1) {
+
+        var char = characters[i];
+
+      if (char === ' ') continue;
+
+      if (obj[char]) {
+        obj[char] += 1;
+    }
+      else {
+        obj[char] =1
+      }
+    }
+    return obj
+    console.log(obj)
+      }
+
+      console.log(countLetters("lighthouse in the house"))
+
+// }
